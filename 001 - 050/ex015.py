bd2454a24134500343ece91b936c169d3a66f89e
Antonio Marcos <a.marcossouto08@gmail.com>
2022-04-21 18:14:37 -0300

    Correção ortográfica em ex015.py

--- 001 - 050/ex015.py
+++ 001 - 050/ex015.py
@@ -1,4 +1,4 @@
-# Códiog Original
+# Código Original
 # dias = int(input('Quantos dias você está com o carro? '))
 # km = float(input('Quantos KM você correu com o carro? '))
 # print(f'Você terá que pagar R${(dias*60)+(km*0.15):.2f} pelo aluguel do carro')
